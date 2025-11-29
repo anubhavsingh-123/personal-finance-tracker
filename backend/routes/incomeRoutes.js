d01d3907410b5b5994d137dd687683e1099ed697
Anubhav Singh <155409817+anubhavsingh-123@users.noreply.github.com>
2025-11-29 18:59:53 +0530

    Update incomeRoutes.js

--- backend/routes/incomeRoutes.js
+++ backend/routes/incomeRoutes.js
@@ -4,7 +4,7 @@ const {
     getAllIncome,
     deleteIncome,
     downloadIncomeExcel
-} = require("../controllers/IncomeController");
+} = require("../controllers/incomeController");
 const { protect } = require("../middleware/authMiddleware");
 
 const router = express.Router();
